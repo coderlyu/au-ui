159d68210e5d08294428f8cdeacb0ddff4513fff
刘誉 <974257574@qq.com>
2021-08-24 14:46:14 +0800

    refactor: 修改

--- test/index.js
+++ test/index.js
@@ -1,7 +1,7 @@
 // require('babel-regenerator-runtime')
 
 // require all test files (files that ends with .spec.js)
-const testsContext = require.context('./specs', true, /\.spec.js$/)
+const testsContext = require.context('./specs', true, /\.spec\.js$/)
 testsContext.keys().forEach(testsContext)
 
 const srcContext = require.context('../src', true, /\.js$/)
